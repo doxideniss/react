59740ae1b2481a4634e487a9678f1da33720cfb7
Deniss <doxideniss@gmail.com>
2020-02-13 14:47:42 +0300

    init

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import s from "./MyPosts.module.css";
+import Post from "./Post/Post";
+
+const MyPosts = () => {
+  return (
+    <div>
+      My posts
+      <div>New post</div>
+      <div className={s.posts}>
+        <Post message="Hi"/>
+        <Post message="First post"/>
+      </div>
+    </div>
+  );
+};
+
+export default MyPosts;
---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import s from "./Post.module.css";
+
+const Post = (props) => {
+  return (
+    <div className={s.item}>
+      <img src='https://i.ya-webdesign.com/images/user-avatar-png-7.png' />
+      {props.message}
+      <div>
+        <span>Like</span>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
